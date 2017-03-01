85088459600a05bb2c825191555b69256f93f5f5
meli.oktavia <meli.oktavia@jatis.com>
2017-03-01 08:54:46 +0700

    Add XmppConnection

---
+++ app/src/main/java/com/meliismyself/xmppchatting/XMPPConfig.java
@@ -0,0 +1,14 @@
+package com.meliismyself.xmppchatting;
+
+/**
+ * Created by meli.oktavia on 01/03/2017.
+ */
+
+public class XMPPConfig {
+    public static final String HOST = "nyooi.com";
+    public static final String SERVICE_NAME = "nyooi.com";
+    public static final int XMPP_PORT = 5222; // this port is unsecure, use 5223 if you have ssl certificate
+    public static final String XMPP_RESOURCE="ngobrol";
+    public static final boolean DEBUG = true;
+
+}
---
+++ app/src/main/java/com/meliismyself/xmppchatting/xmpp/XMPPConnection.java
@@ -0,0 +1,8 @@
+package com.meliismyself.xmppchatting.xmpp;
+
+/**
+ * Created by meli.oktavia on 01/03/2017.
+ */
+
+public class XMPPConnection {
+}
